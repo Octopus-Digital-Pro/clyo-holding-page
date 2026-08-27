2ad9f0f41aa3bbf6a155b6071a7908d2a1f57bb2
gzbotii <83651464+gzbotii@users.noreply.github.com>
2026-08-27 10:29:30 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  devToolbar: {
+    enabled: false,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
